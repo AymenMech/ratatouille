03a0a1c41bbad7b2a956382c944cab9155560e59
AymenMech <aymen.mechiche@gmail.com>
2016-09-12 12:52:23 +0200

    ajout des links dans header

--- inc/header.php
+++ inc/header.php
@@ -2,6 +2,10 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" hrefphp="style/style.css">
+
     <title>
     <?php
     echo $titre;
@@ -17,7 +21,7 @@
       <!-- <li><a href="">Nos restaurants</a></li> -->
       <li><a href="about.php">Qui sommes-nous ?</a></li>
       <li><a href="franchise.php">Franchise</a></li>
-    <!--  <li><a href=""></a></li>  --> 
+    <!--  <li><a href=""></a></li>  -->
     </ul>
   </nav>
 </header>
