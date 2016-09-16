bafb653252e03984f1fa4b13a4779f34e5226f2c
AymenMech <aymen.mechiche@gmail.com>
2016-09-16 13:54:32 +0200

    correction bug afichage

--- index.php
+++ index.php
@@ -3,53 +3,51 @@
 $title= "Accueil";
 include"inc/header.php";
 ?>
-
+  <div class="hidden-xs hidden-sm">
     <div id="carousel-example" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example" data-slide-to="1"></li>
-    <li data-target="#carousel-example" data-slide-to="2"></li>
-  </ol>
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example" data-slide-to="1"></li>
+        <li data-target="#carousel-example" data-slide-to="2"></li>
+      </ol>
 
-  <div class="carousel-inner">
-    <div class="item active">
+      <div class="carousel-inner">
+        <div class="item active">
 
-      <a href="franchise.php" ><img src="images/slide1.jpg" alt="image presentation caroussel" /></a>
-      <div class="carousel-caption">
+          <a href="franchise.php" ><img src="images/slide1.jpg" alt="image presentation caroussel" /></a>
+          <div class="center-block carousel-caption">
 
-        <h3>Cuisine et tradition</h3>
-        <p>Local, bio et bon !</p>
+            <h3>Cuisine et tradition</h3>
+            <p>Local, bio et bon !</p>
+          </div>
+        </div>
+        <div class="item">
+          <a href="restaurant.php"><img src="images/slide2.jpg" alt="image presentation caroussel"/></a>
+          <div class="center-block carousel-caption">
+
+            <h3>Ouverture d'un nouvel établissement !</h3>
+            <p>Découvez ce restaurant à Toulouse</p>
+          </div>
+        </div>
+        <div class="item">
+
+          <a href="restaurant.php"><img src="images/slide3.jpg" alt="image presentation caroussel"/></a>
+          <div class="center-block carousel-caption">
+
+            <h3>Ouverture d'un nouvel établissement !</h3>
+            <p>Découvez ce restaurant à Lyon</p>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="item">
-      <a href="restaurant.php"><img src="images/slide2.jpg" alt="image presentation caroussel"/></a>
-      <div class="carousel-caption">
 
-        <h3>Ouverture d'un nouvel établissement !</h3>
-        <p>Découvez ce restaurant à Toulouse</p>
-      </div>
-    </div>
-    <div class="item">
-
-      <a href="restaurant.php"><img src="images/slide3.jpg" alt="image presentation caroussel"/></a>
-      <div class="carousel-caption">
-
-        <h3>Ouverture d'un nouvel établissement !</h3>
-        <p>Découvez ce restaurant à Lyon</p>
-      </div>
+      <a class="left carousel-control" href="#carousel-example" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+      </a>
+      <a class="right carousel-control" href="#carousel-example" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+      </a>
     </div>
   </div>
-
-  <a class="left carousel-control" href="#carousel-example" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-  </a>
-</div>
-
-<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js'></script>
 
 
 <div class="bloc-index container">
